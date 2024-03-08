53570472aed06f48cbcdefc3b497d194d372ec81
saklani <50768838+saklani@users.noreply.github.com>
2024-03-08 15:27:20 +0530

    ao connect function implemeentation

--- ao.go
+++ ao.go
@@ -1,1 +1,67 @@
 package argo
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+const (
+	MU_URL    = "https://mu.ao-testnet.xyz"
+	CU_URL    = "https://cu.ao-testnet.xyz"
+	GATEWAY   = "https://arweave.net"
+	AO_MODULE = ""
+)
+
+func SendMessage(process string, data string, tags []Tag, anchor string, s *Signer) (string, error) {
+	dataItem, err := NewDataItem([]byte(data), *s, process, anchor, tags)
+	if err != nil {
+		return "", err
+	}
+	resp, err := http.Post(MU_URL+"/monitor/"+process, "application/octet-stream", bytes.NewBuffer(dataItem.Raw))
+	if err != nil {
+		return "", err
+	}
+	res, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var messageId string
+	err = json.Unmarshal(res, &messageId)
+	if err != nil {
+		return "", err
+	}
+	return messageId, nil
+}
+
+type ReadResultResponse struct {
+	Messages []any
+	Spawns   []any
+	Outputs  []any
+	Errors   any
+	GasUsed  int
+}
+
+func ReadResult(process string, message string) (*ReadResultResponse, error) {
+	resp, err := http.Get(MU_URL + "/result/" + message + "?process-id=" + process)
+	if err != nil {
+		return nil, err
+	}
+	res, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var readResult *ReadResultResponse
+	err = json.Unmarshal(res, readResult)
+	if err != nil {
+		return nil, err
+	}
+	return readResult, nil
+}
+
+func SpawnProcess() {
+
+}
---
+++ ao_test.go
@@ -0,0 +1,15 @@
+package argo
+
+import "testing"
+
+func TestSpawnProcess(t *testing.T) {
+	
+}
+
+func TestSendMessage(t *testing.T) {
+	
+}
+
+func TestReadResult(t *testing.T) {
+	
+}
