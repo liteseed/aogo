2f057d56ce298a8d0aafb5fda892ec50fc6e04b0
saklani <50768838+saklani@users.noreply.github.com>
2024-06-13 13:38:11 +0530

    refactor test

--- cu_test.go
+++ cu_test.go
@@ -17,98 +17,100 @@ func NewCUMock(URL string) CU {
 		url:    URL,
 	}
 }
-func TestLoadResult0(t *testing.T) {
-	process := "W7Ax6G1i3C4ksRRNP4Urxvq9bcSmwBK9J0S3QBt9J70"
-	message := "ahcFiWM5RMcXDA-OrAdpjK10Afty6qxvELa83mMbxI0"
-	messages := []map[string]any{{
-		"Target": "F7fmxSBJx5RlIRrt825iIEAL110cKP2Bf8tYd0Q1STU",
-		"Anchor": "00000000000000000000000000000043",
-		"Data":   "{\"wjvyv-Z36LbY8y0UZ21dhzygU56GdqaDqFdT9rq-GPc\":{\"stakedAt\":1363029,\"amount\":200}}",
-		"Tags": []any{
-			map[string]any{
-				"value": "ao",
-				"name":  "Data-Protocol",
-			},
-			map[string]any{
-				"value": "ao.TN.1",
-				"name":  "Variant",
-			},
-			map[string]any{
-				"value": "Message",
-				"name":  "Type",
-			},
-			map[string]any{
-				"value": "W7Ax6G1i3C4ksRRNP4Urxvq9bcSmwBK9J0S3QBt9J70",
-				"name":  "From-Process",
-			},
-			map[string]any{
-				"value": "2rEYpGAF-zuvgKh8-7fie7TLUdXCS1ZHa7GJ_lw3jpo",
-				"name":  "From-Module",
-			},
-			map[string]any{
-				"value": "43",
-				"name":  "Ref_",
-			}},
-	}}
+func TestLoadResult(t *testing.T) {
+	t.Run("0", func(t *testing.T) {
+		process := "W7Ax6G1i3C4ksRRNP4Urxvq9bcSmwBK9J0S3QBt9J70"
+		message := "ahcFiWM5RMcXDA-OrAdpjK10Afty6qxvELa83mMbxI0"
+		messages := []map[string]any{{
+			"Target": "F7fmxSBJx5RlIRrt825iIEAL110cKP2Bf8tYd0Q1STU",
+			"Anchor": "00000000000000000000000000000043",
+			"Data":   "{\"wjvyv-Z36LbY8y0UZ21dhzygU56GdqaDqFdT9rq-GPc\":{\"stakedAt\":1363029,\"amount\":200}}",
+			"Tags": []any{
+				map[string]any{
+					"value": "ao",
+					"name":  "Data-Protocol",
+				},
+				map[string]any{
+					"value": "ao.TN.1",
+					"name":  "Variant",
+				},
+				map[string]any{
+					"value": "Message",
+					"name":  "Type",
+				},
+				map[string]any{
+					"value": "W7Ax6G1i3C4ksRRNP4Urxvq9bcSmwBK9J0S3QBt9J70",
+					"name":  "From-Process",
+				},
+				map[string]any{
+					"value": "2rEYpGAF-zuvgKh8-7fie7TLUdXCS1ZHa7GJ_lw3jpo",
+					"name":  "From-Module",
+				},
+				map[string]any{
+					"value": "43",
+					"name":  "Ref_",
+				}},
+		}}
 
-	handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		data, err := json.Marshal(map[string]any{"Messages": messages, "Spawns": []any{}, "Outputs": []any{}, "GasUsed": 599159077})
+		d, err := json.Marshal(map[string]any{"Messages": messages, "Spawns": []any{}, "Outputs": []any{}, "GasUsed": 599159077})
 		assert.NoError(t, err)
-		_, err = w.Write(data)
+
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodGet, r.Method)
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write(d)
+			assert.NoError(t, err)
+		}))
+
+		defer srv.Close()
+
+		ao := &AO{cu: newCU(srv.URL)}
+
+		res, err := ao.LoadResult(process, message)
 		assert.NoError(t, err)
+		assert.Equal(t, messages[0]["Target"], res.Messages[0]["Target"].(string))
+		assert.Equal(t, messages[0]["Anchor"], res.Messages[0]["Anchor"].(string))
+		assert.Equal(t, messages[0]["Data"], res.Messages[0]["Data"].(string))
+		assert.ElementsMatch(t, messages[0]["Tags"], res.Messages[0]["Tags"])
+		assert.Equal(t, res.GasUsed, 599159077)
 	})
+	t.Run("1", func(t *testing.T) {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodGet, r.Method)
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(`{"Messages": [], "Spawns": [], "Outputs": [], "Error": "", "GasUsed": 0}`))
+			assert.NoError(t, err)
+		}))
+		defer srv.Close()
 
-	http.HandleFunc("/result/"+message, handlerFunc)
+		ao := &AO{cu: newCU(srv.URL)}
 
-	ts := httptest.NewServer(nil)
-	defer ts.Close()
-
-	cu := NewCUMock(ts.URL)
-
-	res, err := cu.LoadResult(process, message)
-	assert.NoError(t, err)
-	assert.Equal(t, messages[0]["Target"], res.Messages[0]["Target"].(string))
-	assert.Equal(t, messages[0]["Anchor"], res.Messages[0]["Anchor"].(string))
-	assert.Equal(t, messages[0]["Data"], res.Messages[0]["Data"].(string))
-	assert.ElementsMatch(t, messages[0]["Tags"], res.Messages[0]["Tags"])
-	assert.Equal(t, res.GasUsed, 599159077)
-}
-
-func TestLoadResult1(t *testing.T) {
-	cuServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, http.MethodGet, r.Method)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"Messages": [], "Spawns": [], "Outputs": [], "Error": "", "GasUsed": 0}`))
-	}))
-	defer cuServer.Close()
-
-	ao := &AO{cu: newCU(cuServer.URL)}
-
-	resp, err := ao.LoadResult("process", "message")
-	assert.NoError(t, err)
-	assert.NotNil(t, resp)
-	assert.Equal(t, 0, resp.GasUsed)
+		resp, err := ao.LoadResult("process", "message")
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+		assert.Equal(t, 0, resp.GasUsed)
+	})
 }
 
 func TestDryRun(t *testing.T) {
-	cuServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, http.MethodPost, r.Method)
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte(`{"Messages": [], "Spawns": [], "Outputs": [], "Error": "", "GasUsed": 0}`))
 		assert.NoError(t, err)
 	}))
-	defer cuServer.Close()
+	defer srv.Close()
 
-	ao := &AO{cu: newCU(cuServer.URL)}
+	ao := &AO{cu: newCU(srv.URL)}
 
-	message := Message{
+	m := Message{
 		ID:     "testID",
 		Target: "testTarget",
 		Owner:  "testOwner",
 		Data:   "testData",
 		Tags:   []tag.Tag{},
 	}
-	resp, err := ao.DryRun(message)
+	resp, err := ao.DryRun(m)
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
 	assert.Equal(t, 0, resp.GasUsed)
--- mu_test.go
+++ mu_test.go
@@ -18,84 +18,88 @@ func NewMUMock(URL string) MU {
 	}
 }
 
-func TestSendMessage0(t *testing.T) {
-	process := "yugMfaR-u_11GkAuZhqeChPuzoxVYuJW8RnNCIby-D8"
-	data := ""
-	tags := []tag.Tag{{Name: "Action", Value: "Stakers"}}
+func TestSendMessage(t *testing.T) {
+	t.Run("0", func(t *testing.T) {
+		process := "yugMfaR-u_11GkAuZhqeChPuzoxVYuJW8RnNCIby-D8"
+		data := ""
+		tags := []tag.Tag{{Name: "Action", Value: "Stakers"}}
 
-	s, err := signer.FromPath("./keys/wallet.json")
-	assert.NoError(t, err)
-	muServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, http.MethodPost, r.Method)
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte(`{"id": "mockMessageID"}`))
+		s, err := signer.FromPath("./keys/wallet.json")
 		assert.NoError(t, err)
-	}))
-	defer muServer.Close()
+		muServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPost, r.Method)
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(`{"id": "mockMessageID"}`))
+			assert.NoError(t, err)
+		}))
+		defer muServer.Close()
 
-	mu := NewMUMock(muServer.URL)
-	res, err := mu.SendMessage(process, data, tags, "", s)
-	assert.NoError(t, err)
-	assert.True(t, res != "")
+		mu := NewMUMock(muServer.URL)
+		res, err := mu.SendMessage(process, data, tags, "", s)
+		assert.NoError(t, err)
+		assert.True(t, res != "")
+	})
+
+	t.Run("1", func(t *testing.T) {
+		muServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPost, r.Method)
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(`{"id": "mockMessageID"}`))
+			assert.NoError(t, err)
+		}))
+		defer muServer.Close()
+
+		ao := &AO{mu: newMU(muServer.URL)}
+
+		signer, err := signer.FromPath("./keys/wallet.json") // Mock signer or use a real one for the test
+		assert.NoError(t, err)
+
+		id, err := ao.SendMessage("process", "data", nil, "", signer)
+		assert.NoError(t, err)
+		assert.Equal(t, "mockMessageID", id)
+	})
 }
 
-func TestSendMessage1(t *testing.T) {
-	muServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, http.MethodPost, r.Method)
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte(`{"id": "mockMessageID"}`))
+func TestSpawnProcess(t *testing.T) {
+	t.Run("0", func(t *testing.T) {
+		muServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPost, r.Method)
+			w.WriteHeader(http.StatusAccepted)
+			_, err := w.Write([]byte(`{"id": "mockMessageID"}`))
+			assert.NoError(t, err)
+		}))
+		defer muServer.Close()
+
+		mu := NewMUMock(muServer.URL)
+
+		data := ""
+		tags := []tag.Tag{{Name: "Action", Value: "Stakers"}}
+
+		s, err := signer.FromPath("./keys/wallet.json")
 		assert.NoError(t, err)
-	}))
-	defer muServer.Close()
 
-	ao := &AO{mu: newMU(muServer.URL)}
+		res, err := mu.SpawnProcess("", data, tags, s)
 
-	signer, err := signer.FromPath("./keys/wallet.json") // Mock signer or use a real one for the test
-	assert.NoError(t, err)
-
-	id, err := ao.SendMessage("process", "data", nil, "", signer)
-	assert.NoError(t, err)
-	assert.Equal(t, "mockMessageID", id)
-}
-
-func TestSpawnProcess0(t *testing.T) {
-	muServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, http.MethodPost, r.Method)
-		w.WriteHeader(http.StatusAccepted)
-		_, err := w.Write([]byte(`{"id": "mockMessageID"}`))
 		assert.NoError(t, err)
-	}))
-	defer muServer.Close()
+		assert.True(t, res != "")
+	})
 
-	mu := NewMUMock(muServer.URL)
+	t.Run("1", func(t *testing.T) {
+		muServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPost, r.Method)
+			w.WriteHeader(http.StatusAccepted)
+			_, err := w.Write([]byte(`{"id": "mockProcessID"}`))
+			assert.NoError(t, err)
+		}))
+		defer muServer.Close()
 
-	data := ""
-	tags := []tag.Tag{{Name: "Action", Value: "Stakers"}}
+		ao := &AO{mu: newMU(muServer.URL)}
 
-	s, err := signer.FromPath("./keys/wallet.json")
-	assert.NoError(t, err)
-
-	res, err := mu.SpawnProcess("", data, tags, s)
-
-	assert.NoError(t, err)
-	assert.True(t, res != "")
-}
-
-func TestSpawnProcess1(t *testing.T) {
-	muServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, http.MethodPost, r.Method)
-		w.WriteHeader(http.StatusAccepted)
-		_, err :=  w.Write([]byte(`{"id": "mockProcessID"}`))
+		signer, err := signer.FromPath("./keys/wallet.json") // Mock signer or use a real one for the test
 		assert.NoError(t, err)
-	}))
-	defer muServer.Close()
 
-	ao := &AO{mu: newMU(muServer.URL)}
-
-	signer, err := signer.FromPath("./keys/wallet.json") // Mock signer or use a real one for the test
-	assert.NoError(t, err)
-
-	id, err := ao.SpawnProcess("module", "data", nil, signer)
-	assert.NoError(t, err)
-	assert.Equal(t, "mockProcessID", id)
+		id, err := ao.SpawnProcess("module", "data", nil, signer)
+		assert.NoError(t, err)
+		assert.Equal(t, "mockProcessID", id)
+	})
 }
